fe5fc62377a7139893f772f4d2692a7ddf6c668c
SakuraHitachi <SakuraHitachi>
2017-03-20 15:29:01 +0600

    добавила карточку товара

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 /**
  * Created by Александра on 15.02.2017.
  */
+$(document).ready(function () {
+
+    $('.slider-main').slick({
+        slidesToShow: 1,
+        asNavFor: '.slider-nav',
+        arrows: false,
+        centerMode: true,
+        centerPadding: '10px'
+    });
+
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        asNavFor: '.slider-main',
+        centerMode: true,
+        centerPadding: '1px'
+    });
+
+    $('.colors .circle').click(function () {
+       var index = $(this).attr('data-slide') ;
+       console.log(index);
+        $('.slider-nav').slick('slickGoTo', index);
+    });
+});
